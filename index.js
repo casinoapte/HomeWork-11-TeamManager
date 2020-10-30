23815c5a5bade914a6be930d3c1dfef7416e9551
Casino Apte <casino.apte@gmail.com>
2020-10-29 22:27:59 -0600

    fixed some bugs. work remains

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // REQUIREMENTS //
 const Employee = require("./lib/employee")
 const Role = require("./lib/role")
+const Department = require("./lib/department")
+
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const path = require("path");
@@ -17,12 +19,15 @@ function menu() {
                 type: "list",
                 name: "userAction",
                 message: "What would you like to do?",
-                choices: ["View Employees", "Add Employees", "Delete Employees", "Edit Employees"]
+                choices: ["View Employees", "View Departments", "Add Employees", "Delete Employees", "Edit Employees"]
             }
         ]).then((response) => {
             switch (response.userAction) {
                 case "View Employees":
                     viewEmployee();
+                    break
+                    case "View Departments":
+                    viewDepartment();
                     break
                 case "Add Employees":
                     addEmployee();
@@ -49,9 +54,21 @@ function menu() {
             prompt();
         })
     }
-    
-    "SELECT employee.id, employee.firstname, employee.lastname, employee.manager_id, department.depart_name, department.id FROM employee INNER JOIN department ON employee.id = department.id"
 
+    // VIEW DEPARTMENT //
+
+    function viewDepartment() {
+        var query = connection.query( "SELECT employee.id, employee.firstname, employee.lastname, department.depart_name FROM employee INNER JOIN department ON employee.id = department.id", function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            prompt();
+        })
+    }
+
+
+
+
+    
     // ADD EMPLOYEE //
 
     function addEmployee() {
@@ -88,6 +105,11 @@ function menu() {
             },
             {
                 type: "input",
+                name: "departName",
+                message: "What is this employees department Name?"
+            },
+            {
+                type: "input",
                 name: "roleDepartID",
                 message: "What is this employees department ID?"
             },
@@ -99,10 +121,15 @@ function menu() {
             console.log("Added Employee to Database...");
             const newEmployee = new Employee(res.employeeFirstName, res.employeeLastName, res.employeeRole, res.employeeManager)
             const newRole = new Role(res.roleTitle, res.roleSalary, res.roleDepartID)
+            const newDep = new Department(res.departName)
+
             var query = connection.query("INSERT INTO employee SET ?", newEmployee, function (err, res) {
                 if (err) throw err;
             })
             var query = connection.query("INSERT INTO role SET ?", newRole, function (err, res) {
+                if (err) throw err;
+            })
+            var query = connection.query("INSERT INTO department SET ?", newDep, function (err, res) {
                 if (err) throw err;
             })
             prompt();
@@ -122,9 +149,12 @@ function menu() {
         ]).then((res) => {
 
             var remEmployee = res.employeeRemove
-            var query = connection.query("DELETE  FROM employee WHERE firstname =? ", remEmployee, function (err, res) {
+            var query = connection.query("DELETE FROM employee WHERE firstname =? ", remEmployee, function (err, res) {
                 if (err) throw err;
             })
+            // var query = connection.query("DELETE  FROM role WHERE firstname =? ", remEmployee, function (err, res) {
+            //     if (err) throw err;
+            // })
             prompt();
         })
 
